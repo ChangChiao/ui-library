5273ed1319216a57e5b476af1e2980dd7b1157aa
JoeCT_Chang <JoeCT.Chang@moxa.com>
2024-01-31 09:17:00 +0800

    feat: add libs avatar

---
+++ ui-avatar/src/lib/ui-avatar/ui-avatar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ui-library-ui-avatar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ui-avatar.component.html',
+  styleUrl: './ui-avatar.component.scss',
+})
+export class UiAvatarComponent {}
